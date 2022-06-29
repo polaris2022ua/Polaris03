21902cc4ef5918b795858da0339e539181504972
Lertss <cEntur!0>
2022-06-30 00:36:28 +0200

    Polaris 1.0003 "сделано нормальную модель с email, username, phone, password, date_joined -дата первого входа, last_login-дата последнего входа, is_admin, is_active, is_staff, is_superuser,profile_image,hide_email- cокрытие эмаила"
    вход происходит через имаил.
    
    работает логин и logout
    
    сделана реестрация!!!!!!!!!!!!!!!!!!!!! Работает

---
+++ account/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+class UserCreationForm(UserCreationForm):
+    email = forms.EmailField(label=_("Email"), max_length=100, widget=forms.EmailInput(attrs={"autocomplete": "email"}))
+    username = forms.CharField(label=_("Username"),max_length=40, )
+    phone = forms.CharField(label=_("Phone"),max_length=15,)
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("username", "email", "phone")
--- account/views.py
+++ account/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views import View
+from account.forms import UserCreationForm
 
-# Create your views here.
 
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, requset):
+        context = {
+            'form': UserCreationForm()
+        }
+        return render(requset, self.template_name, context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # email = form.cleaned_data.get('email')
+            # phone = form.cleaned_data.get('phone')
+            # username = form.cleaned_data.get('username')
+            # password = form.cleaned_data.get('password1')
+            # user = authenticate(username=username, email=email, phone=phone, password=password)
+            user = form.save()
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('home')
+        context = {
+                'form': form
+            }
+        return render(request, self.template_name, context)
